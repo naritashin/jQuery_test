4593519ea8600a027b569378e7f3f97ecb539682
SHIN_NARITA <triplfjump1417@gmail.com>
2017-11-08 13:02:24 +0900

    2017-11-08　午前報

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -3,6 +3,12 @@ $(function() {
       $regionList = $('.p-search_region_list'),
       $refineList = $('.p-search_refine_list');
 
+      var json,
+          appgenre,
+          appatmosphere,
+          Apphotword
+          arr = {"refineId": []};
+
   $('.p-search_btn').on('click', function() {
     $search.slideToggle(100);
   });
@@ -45,7 +51,7 @@ $(function() {
     .slideToggle(100);
   });
 
-    $refineList.on('click', 'li p', function() {
+  $refineList.on('click', 'li p', function() {
     $(this).parent()
     .siblings()
     .find('ul')
@@ -64,12 +70,42 @@ $(function() {
   });
 
   $refineList.on('change', 'input', function() {
+    var checked = $refineList.find('input:checked'),
+        txt = checked.length === 0 ? '(条件未設定)' : '';
+
     $(this).next()
-    .children()
+    .children('.p-search_checkbox')
     .toggleClass('is-checked');
+
+    $('.js-refine')
+    .text(txt);
+
+    checked.each(function() {
+      var item = $(this).val()+',';
+
+      $('.js-refine').append(item);
+    });
   });
 
+  $('.js-region-reset').on('click', function() {
+    $regionList.find('input:checked')
+    .prop('checked', false)
+    .next()
+    .find('.is-checked')
+    .removeClass('is-checked');
 
+    $('.js-cities').text('(条件未設定)');
+  });
+
+  $('.js-refine-reset').on('click', function() {
+    $refineList.find('input:checked')
+    .prop('checked', false)
+    .next()
+    .find('.is-checked')
+    .removeClass('is-checked');
+
+    $('.js-refine').text('(条件未設定)');
+  });
 
   $.ajax({
     url: '../../api/cities.json',
@@ -77,6 +113,7 @@ $(function() {
   })
   .done(function(data) {
     setRegion(data);
+    createRestaurantRegion(data);
   })
   .fail(function() {
     console.log("error");
@@ -90,7 +127,9 @@ $(function() {
     dataType: 'json',
   })
   .done(function(data) {
-    setGenre(data);
+    setRefine(data, "genre");
+
+    createRestaurantRefine(data, "genre");
   })
   .fail(function() {
     console.log("error");
@@ -104,7 +143,8 @@ $(function() {
     dataType: 'json',
   })
   .done(function(data) {
-    setAtmosphere(data);
+    setRefine(data, "atmosphere");
+    createRestaurantRefine(data, "atmosphere");
   })
   .fail(function() {
     console.log("error");
@@ -118,7 +158,8 @@ $(function() {
     dataType: 'json',
   })
   .done(function(data) {
-    setHotword(data);
+    setRefine(data, "hotword");
+    createRestaurantRefine(data, "hotword");
   })
   .fail(function() {
     console.log("error");
@@ -126,7 +167,6 @@ $(function() {
   .always(function() {
     console.log("complete");
   });
-  
 
   function setRegion(data) {
     data.places.forEach(function(places) {
@@ -142,28 +182,88 @@ $(function() {
     });
 
     $('.p-search_cities_list').on('change', 'input', function() {
+      var checked = $regionList.find('input:checked'),
+          txt = checked.length === 0 ? '(条件未設定)' : '';
+
       $(this).next()
       .children('.p-search_checkbox')
       .toggleClass('is-checked');
+
+      $('.js-cities')
+      .text(txt);
+
+      checked.each(function() {
+        var item = $(this).val()+',';
+
+        $('.js-cities').append(item);
+      });
     });
   }
 
   function setCities(place) {
     place.cities.forEach(function(cities, i) {
-      var cityId = i + place.id,
+      var num = i + place.id + 1,
           item =
             '<li>'+
-              '<input type="checkbox" id="'+ cityId +'" value="'+ cityId +'">'+
-              '<label for="'+ cityId +'" class="p-search_label">'+
+              '<input type="checkbox" id="'+ num +'" value="'+ cities +'">'+
+              '<label for="'+ num +'" class="p-search_label">'+
                 '<span class="p-search_checkbox"></span><span class="p-search_cities_name">'+ cities +'</span>(num)'+
               '</label>'+
             '</li>';
 
-      $('.js-region_'+ place.id).append(item);
+      $('.js-region_' + place.id).append(item);
     });
   }
 
-  $('html').on('change', 'input:checked', function() {
-    console.log($('input:checked'));
+  function setRefine(data, val) {
+    data[val].forEach(function(value) {
+      var item =
+          '<li>'+
+            '<input type="checkbox" id="'+ value.id +'" value="'+ value.name +'"><label for="'+ value.id +'" class="p-search_label">'+
+              '<span class="p-search_checkbox"></span><span class="p-search_'+ val +'_name">'+ value.name +'</span>(num)'+
+            '</label>'+
+          '</li>';
+
+      $('.p-search_'+ val +'_list').append(item);
+    })
+  }
+
+  function createRestaurantRegion(data) {
+    item1 = data.places[Math.floor(Math.random()*data.places.length)];
+    regionId = item1.id;
+    cityName = item1.cities[Math.floor(Math.random()*item1.cities.length)];
+
+    json = {"regionId": regionId, "cityName": cityName};
+
+    appRegion ='{<br>"regionId": '+json.regionId+',<br>"cityName": "'+json.cityName+'"<br>},<br>';
+  }
+
+  function createRestaurantRefine(data, type) {
+    num = Math.floor(Math.random()*4);
+
+    for(var i = 0; i <= num; i++) {
+      var id = data[type][Math.floor(Math.random()*data[type].length)].id;
+
+      arr.refineId.push(id);
+      console.log(arr);
+
+    }
+  }
+
+  function createRestaurant() {
+    var restaurant = Object.assign(json, arr);
+    console.log(restaurant);
+    var item =
+    '{<br>'+
+    '"regionId": '+restaurant.regionId+',<br>'+
+    '"cityName": "'+restaurant.cityName+'",<br>'+
+    '"refineId": ['+restaurant.refineId+']<br>'+
+    '},<br>';
+
+    $('footer').append(item);
+  }
+
+  $('html').on('click', function() {
+    createRestaurant();
   });
 });
